f30bd72867838a19da24db0c84fe1b29028f9bef
yamajid <yamajid@student.1337.ma>
2026-04-17 18:58:25 +0100

    added deploy.yml and configure site to deploy on github pages

--- astro_new_arch/astro.config.mjs
+++ astro_new_arch/astro.config.mjs
@@ -5,7 +5,9 @@ import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
-  site: 'https://v0-guide.com',
+  site: 'https://yamajid.github.io/v0-site-2/',
+  base: '/v0-site-2/',
+  output: 'static',
   integrations: [
     react(),
     tailwind({
